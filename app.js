f36b547f63c4caafe94f4498dc8489a92f4dba42
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-12 17:10:56 +0700

    models place

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+
+// Models
+const Place = require("./models/place");
+
+mongoose
+  .connect("mongodb://localhost:27017/bestpoints")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/seed/place", async (req, res) => {
+  const place = new Place({
+    title: "Empire State Building",
+    price: "$999999",
+    description: "A Great building",
+    location: "New York, NY",
+  });
+  await place.save();
+  res.send(place);
 });
 
 app.listen(3000, () => {
