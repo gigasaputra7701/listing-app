72e84bb5e9750798a105015d3a0954b43f650d89
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-12 17:26:54 +0700

    template seeds

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/seed/place", async (req, res) => {
-  const place = new Place({
-    title: "Empire State Building",
-    price: "$999999",
-    description: "A Great building",
-    location: "New York, NY",
-  });
-  await place.save();
-  res.send(place);
-});
+// app.get("/seed/place", async (req, res) => {
+//   const place = new Place({
+//     title: "Empire State Building",
+//     price: "$999999",
+//     description: "A Great building",
+//     location: "New York, NY",
+//   });
+//   await place.save();
+//   res.send(place);
+// });
 
 app.listen(3000, () => {
   console.log(`server is running on http://127.0.0.1:3000`);
