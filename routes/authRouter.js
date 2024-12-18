1be6455f4749a2b534cc0d6e5026f1ae3d472663
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-19 01:14:52 +0700

    middleware isValidObjectId

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const isValidObjectId = require("../middleware/isValidObjectId");
 
 const {
   getPlaces,
@@ -18,14 +19,18 @@ router.get("/", getPlaces);
 router.post("/", postPlaces);
 
 router.get("/create", getCreate);
-router.get("/:id", getDetailsPlace);
-router.get("/:id/edit", getEdit);
-router.put("/:id", putEdit);
-router.delete("/:id", deletePlace);
+router.get("/:id", isValidObjectId("/places"), getDetailsPlace);
+router.get("/:id/edit", isValidObjectId("/places"), getEdit);
+router.put("/:id", isValidObjectId("/places"), putEdit);
+router.delete("/:id", isValidObjectId("/places"), deletePlace);
 
 // Restful Review
-router.post("/:id/reviews", postReview);
-router.delete("/:id/reviews/:review_id", deleteReview);
+router.post("/:id/reviews", isValidObjectId("/places"), postReview);
+router.delete(
+  "/:id/reviews/:review_id",
+  isValidObjectId("/places"),
+  deleteReview
+);
 
 // 404 Handler
 router.all("*", pageNotFound);
